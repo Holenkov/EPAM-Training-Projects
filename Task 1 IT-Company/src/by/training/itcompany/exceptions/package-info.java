803f65c61219f0e99de8eef793bc549c0f0bc495
Holenkov <andrey.holenkov@gmail.com>
2019-05-12 19:40:31 +0300

    remove

--- Task 1 IT-Company/src/by/training/itcompany/exceptions/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
-* Package with Custom Exceptions.
-*/
-
-package by.training.itcompany.exceptions;
--- Task 1 IT-Company/src/by/training/itcompany/factories/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
-* Package with factories for making entities.
-*/
-
-package by.training.itcompany.factories;
